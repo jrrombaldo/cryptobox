a15aa1909875132c4394926b718df65b2758afde
jr.rombaldo <jr.rombaldo@gmail.com>
2019-12-05 07:28:59 +0000

    mount/umount is working, but keychain still manual

--- src/scripts/UIHelper.js
+++ src/scripts/UIHelper.js
@@ -8,6 +8,22 @@ function getDirectoryNatively() {
     return directory
 }
 
+function confirmPasswordUse(){
+    const options = {
+        type: 'info',
+        title: 'password confirmation',
+        message: 'There is a pasword recorded on keychain for this folder, would you like reuse or replace? note that the previous password will be lost',
+        buttons: ['Reuse existing password', 'Replace with a new password']
+    };
+    dialog.showMessageBox(options, (index) => {
+        if (index === 0)
+            return true
+        if (index === 1)
+            return false
+
+    })
+}
 
 
-module.exports = {getDirectoryNatively}
+
+module.exports = {getDirectoryNatively, confirmPasswordUse}
