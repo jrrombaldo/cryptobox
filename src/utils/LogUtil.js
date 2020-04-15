686f44d6b337b2355939464e530a7933c6aac352
Carlos Rombaldo Jr <jr.rombaldo@gmail.com>
2020-04-15 15:33:35 +0100

    running electron-packager, forge and builder

--- src/utils/LogUtil.js
+++ src/utils/LogUtil.js
@@ -1,7 +1,7 @@
 const log = require("electron-log");
 // import { log } from "electron-log";
 
-log.transports.file.level = false;
+log.transports.file.level = true;
 log.transports.console.format =
   "[{y}-{m}-{d} {h}:{i}:{s}.{ms}] [{level}] {text}";
 
