3be4a3214e596b91277839882f5edf75cf049ce5
Rafael Badan <rafael.badan@gmail.com>
2020-04-16 19:25:45 -0300

    prevents filepath with special char. to break

--- src/services/encryption/EncryptionServiceOSX.ts
+++ src/services/encryption/EncryptionServiceOSX.ts
@@ -4,7 +4,6 @@ import { EncryptionServiceBase } from "./EncryptionServiceBase";
 
 export class EncryptionServiceOSX extends EncryptionServiceBase
   implements EncryptionService {
-
   getIsMountedCMD(volume: Volume): string {
     return `mount | grep -qs '${volume.decryptedFolderPath}'`;
   }
@@ -15,7 +14,7 @@ export class EncryptionServiceOSX extends EncryptionServiceBase
 
   getMountCMD(volume: Volume, passwordCommand: string): string {
     let impl = "encfs";
-    return `${impl}  ${volume.encryptedFolderPath} ${volume.decryptedFolderPath} --extpass='${passwordCommand}'  --idle=${volume.ttl} --standard --require-macs -ovolname=${volume.name} -oallow_root -olocal -ohard_remove -oauto_xattr -onolocalcaches`;
+    return `${impl} '${volume.encryptedFolderPath}' '${volume.decryptedFolderPath}' --extpass='${passwordCommand}'  --idle=${volume.ttl} --standard --require-macs -ovolname=${volume.name} -oallow_root -olocal -ohard_remove -oauto_xattr -onolocalcaches`;
     // return `${impl}  ${volume.encryptedFolderPath} ${volume.decryptedFolderPath} --extpass='${passwordCommand}';
   }
 }
