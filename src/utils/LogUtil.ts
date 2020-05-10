2fecf757c65b8955524d7992d11153606ee18c1b
Carlos Rombaldo jr <jr.rombaldo@gmail.com>
2020-05-11 00:22:00 +0100

    Fixing log level

--- src/utils/LogUtil.ts
+++ src/utils/LogUtil.ts
@@ -1,8 +1,10 @@
 import log from "electron-log";
 
 const isDev = process.argv0.includes("node_modules");
+if (isDev){
+  log.transports.file.level = (isDev ? false : 'debug');
+}
 
-// log.transports.file.level = isDev ? false : true;
 
 log.transports.console.format =
   "[{y}-{m}-{d} {h}:{i}:{s}.{ms}] [{level}] {text}";
